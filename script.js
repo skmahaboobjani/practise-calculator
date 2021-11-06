c1b74816a4545a9455ace5613aadab336701ee23
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-11-06 12:14:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+var input;
+function funce()
+{
+    document.getElementById("result").value="0";
+}
+function fun1()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "1";
+
+}
+function fun2()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "2";
+
+}
+function fun3()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "3";
+
+}
+function fun4()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "4";
+
+}
+function fun5()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "5";
+
+}
+function fun6()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "6";
+
+}
+function fun7()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "7";
+
+}
+function fun8()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "8";
+
+}
+function fun9()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "9";
+
+}
+function fun0()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "0";
+
+}
+function fundot()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + ".";
+
+}
+function fun_backspace()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input.slice(0,(input.length)-1);
+
+}
+function funplus()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "+";
+
+}
+function funsub()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "-";
+
+}
+function funmul()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "*";
+
+}
+function fundiv()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "/";
+
+}
+function funmod()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=input + "%";
+
+}
+function fun_calculate()
+{
+    input=document.getElementById("result").value;
+    document.getElementById("result").value=eval(input);
+
+}
+
